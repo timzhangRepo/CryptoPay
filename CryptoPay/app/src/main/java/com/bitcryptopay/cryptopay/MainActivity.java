38c5d812afba9bbd030ee80b98a0974a2eaf911a
rdc5230 <rdc5230@psu.edu>
2017-11-04 20:46:48 -0400

    added inital eth api

--- CryptoPay/app/src/main/java/com/bitcryptopay/cryptopay/MainActivity.java
+++ CryptoPay/app/src/main/java/com/bitcryptopay/cryptopay/MainActivity.java
@@ -2,12 +2,42 @@ package com.bitcryptopay.cryptopay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import com.android.volley.*;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private RequestQueue queue;
+
+    public void onTestOneClick(View v) {
+        String url = "https://api.blockcypher.com/v1/eth/main/addrs/738d145faabb1e00cf5a017588a9c0f998318012/balance";
+        JsonObjectRequest r = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    ((TextView)findViewById(R.id.textView)).setText(response.getString("balance"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(r);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        queue = Volley.newRequestQueue(this);
     }
 }
